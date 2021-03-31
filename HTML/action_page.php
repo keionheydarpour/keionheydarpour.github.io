1ba439ec205c99acf2a0804c252e89d36e611102
keionheydarpour <81461941+keionheydarpour@users.noreply.github.com>
2021-03-31 02:23:44 -0500

    Add files via upload

--- HTML/action_page.php
+++ HTML/action_page.php
@@ -1,20 +1,14 @@
 <?php
+if (isset($_POST['submit'])){
   $fname = $_POST['fname']
   $lname = $_POST['lname']
-  $visitor_email = $_POST['email'];
-  $subject = $_POST['subject'];
+  $mailFrom = $_POST['email']
+  $subject = $_POST['subject']
 
-  $email_from = 'keionheydarpour@gmail.com'
+  $mailTo="keionheydarpour@hotmail.com"
+  $headers= "From: ".$mailFrom;
+  $txt = "You have received an email from ".$fname" ".$lname".\n\n".$subject;
 
-  $email_subject = "New Form Submission";
-
-  $email_body = "User Name: $fname $lname. \n".
-                  "User Email: $visitor_email. \n".
-                    "User Mesage: $subject. \n";
-  $to = "keionheydarpour@gmail.com"
-
-  $headers = "From: $email_from \r\n";
-  $headers .= "Reply-To: $visitor_email \r\n";
-  mail($to,$email_subject,$email_body,$headers);
-  header("Location: contact.html")
+  mail($mailTo, $txt, $headers);
+  header("Location: contact.html?mailsend");
 ?>
