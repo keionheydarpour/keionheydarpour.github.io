ba88f2b1038963a6ed44fa47e4de4910a03fdb86
keionheydarpour <81461941+keionheydarpour@users.noreply.github.com>
2021-05-24 14:33:07 -0500

    Add files via upload

--- HTML/zoomin.js
+++ HTML/zoomin.js
@@ -2,7 +2,7 @@ const lightbox = document.createElement('div')
 lightbox.id = 'lightbox'
 document.body.appendChild(lightbox)
 
-const images= document.querySelectorAll('img')
+const images= document.querySelectorAll('.four5 img')
 images.forEach(image => {
   image.addEventListener('click', e => {
     lightbox.classList.add('active')
